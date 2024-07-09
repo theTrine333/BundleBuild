d4bdd553e79459b1f2e3f92c665aa0eb872f256c
Erick Ronald <148716108+theTrine333@users.noreply.github.com>
2024-07-09 06:28:49 +0300

    Update Home.java

--- src/com/cod3r/Home.java
+++ src/com/cod3r/Home.java
@@ -3,6 +3,8 @@ package com.cod3r;
 import java.awt.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
+
+import java.util.Calendar;
 import java.util.concurrent.*;
 import java.awt.Toolkit;
 import javax.swing.*;
@@ -56,7 +58,7 @@ public class Home extends JFrame {
 		});
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Home.class.getResource("/icons/android.png")));
 		setResizable(false);
-		setSize(447,445);
+		setSize(620,447);
 		setLocationRelativeTo(null);
 		setTitle("Android aab to apk");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -70,30 +72,65 @@ public class Home extends JFrame {
 		menuBar.add(mnAbout);
 		
 		JMenuItem mntmSystemsInfo = new JMenuItem("Systems info");
+		mntmSystemsInfo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String aboutText = "BundleBuilder. \n" +
+		        		"-------------------------------------------------------------------------------------\n"+ 
+		        		"Version: 1.0\n" +
+		        		"Author : TrineTechs\n"+ 
+		        		"-------------------------------------------------------------------------------------\n"+
+		        		"This is an opensource system developed by theTrine333. "+
+		        		"The system allows easy and efficient way of converting android bundles to installable apks."+ 
+		        		"\nKey Features include:\n"+
+		        		"-------------------------------------------------------------------------------------\n"+
+		        		"- Graphical Based : The user can just click buttons instead of writing codes\n"+
+		        		"- Android detection : The system is able to uses the installed adb to connect to devices.\n"+
+		        		"- Selective build : The system allows building apks for different android CPUs. \n"+
+		        		"- Autoinstallations : After the apks are build,the system can install them to the connected android device via user authorization. \n" +
+		        		"-------------------------------------------------------------------------------------\n"+
+		        		"For further assistance or inquiries, Please contact the developer: \n"+
+		        		"\tEmail : trinekaka@gmail.com\n"+ 
+		        		String.format("%c ", 169) + Calendar.getInstance().get(Calendar.YEAR) +" BundleBuilder. All rights reserved. \n"+
+		        		"-------------------------------------------------------------------------------------";
+				
+				JOptionPane.showMessageDialog(null, aboutText, "About Moyale", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
 		mnAbout.add(mntmSystemsInfo);
 		
 		JMenuItem mntmDevelopersInfo = new JMenuItem("Developers info");
 		mnAbout.add(mntmDevelopersInfo);
 		
+		JMenuItem mntmEreeyey = new JMenuItem("Ereeyey");
+		mntmEreeyey.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("You clicked");
+			}
+		});
+		mnAbout.add(mntmEreeyey);
+		
 		JMenu mnSettings = new JMenu("Settings");
 		menuBar.add(mnSettings);
 		
+		JMenuItem mntmKeystore = new JMenuItem("Keystore");
+		mnSettings.add(mntmKeystore);
+		
 		panel = new JPanel();
-		panel.setBounds(0, 0, 447, 385);
+		panel.setBounds(0, 0, 614, 385);
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Convert aab to apk easily");
 		lblNewLabel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(26, 95, 180)));
 		lblNewLabel.setFont(UIManager.getFont("h4.font"));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(0, 6, 445, 36);
+		lblNewLabel.setBounds(0, 6, 608, 36);
 		panel.add(lblNewLabel);
 		
 		getContentPane().add(panel);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setCursor(Cursor.getPredefinedCursor(Cursor.TEXT_CURSOR));
-		scrollPane.setBounds(1, 157, 445, 222);
+		scrollPane.setBounds(1, 157, 607, 222);
 		panel.add(scrollPane);
 		
 		textPane = new JTextPane();
@@ -105,17 +142,17 @@ public class Home extends JFrame {
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "Bundle(aab) path", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_1.setBounds(10, 49, 227, 48);
+		panel_1.setBounds(10, 49, 371, 48);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
 		aabPath = new JTextField();
-		aabPath.setBounds(5, 19, 168, 23);
+		aabPath.setBounds(5, 16, 307, 25);
 		panel_1.add(aabPath);
 		aabPath.setColumns(10);
 		
 		JButton btnNewButton = new JButton("");
-		btnNewButton.setBounds(175, 12, 46, 30);
+		btnNewButton.setBounds(319, 16, 46, 25);
 		panel_1.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -136,12 +173,12 @@ public class Home extends JFrame {
 		JPanel panel_1_1 = new JPanel();
 		panel_1_1.setLayout(null);
 		panel_1_1.setBorder(new TitledBorder(null, "apk output dir", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_1_1.setBounds(10, 100, 227, 48);
+		panel_1_1.setBounds(10, 100, 371, 48);
 		panel.add(panel_1_1);
 		
 		outputDir = new JTextField();
 		outputDir.setColumns(10);
-		outputDir.setBounds(5, 19, 168, 23);
+		outputDir.setBounds(5, 16, 307, 25);
 		panel_1_1.add(outputDir);
 		
 		JButton btnNewButton_1 = new JButton("");
@@ -159,12 +196,12 @@ public class Home extends JFrame {
 			}
 		});
 		btnNewButton_1.setIcon(Tools.resizeIcon("/icons/folder.png",20,20));
-		btnNewButton_1.setBounds(175, 12, 46, 30);
+		btnNewButton_1.setBounds(319, 16, 46, 25);
 		panel_1_1.add(btnNewButton_1);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(null, "Device to convert for : ", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_2.setBounds(240, 42, 200, 76);
+		panel_2.setBounds(393, 42, 200, 76);
 		panel.add(panel_2);
 		panel_2.setLayout(null);
 		
@@ -242,7 +279,7 @@ public class Home extends JFrame {
 		btnConvert.setFont(new Font("Liberation Sans", Font.BOLD, 13));
 		btnConvert.setBackground(new Color(0, 0, 255));
 		btnConvert.setForeground(Color.WHITE);
-		btnConvert.setBounds(240, 122, 200, 23);
+		btnConvert.setBounds(393, 122, 200, 23);
 		panel.add(btnConvert);
 		
 		
